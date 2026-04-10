7b9a4f93c18e8ef48ce809ba20050ba7af65bc25
jesusestebanlopezcharry-source <jesusestebanlopezcharry@gmail.com>
2026-04-10 16:20:20 -0500

    Update optional-activity-10.java

--- 10-week/02-optional-activity/optional-activity-10.java
+++ 10-week/02-optional-activity/optional-activity-10.java
@@ -3,20 +3,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-			
-																								   
-									
-								 
-							 
-							 
-		  
-
-		  
-													   
-															  
-							   
-																  
-		  
+		
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("¿Cuántos estudiantes desea ingresar?: ");
